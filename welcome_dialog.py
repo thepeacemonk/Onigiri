0b135c765b77406b933991f67cd3eb01e23c949d
Peace <105552060+thepeacemonk@users.noreply.github.com>
2026-01-24 08:28:59 -0300

    Update 1.0.8.3-beta with PR
    Included a fix made by a collaborator for the Overviewer screen

--- welcome_dialog.py
+++ welcome_dialog.py
@@ -5,7 +5,7 @@ from aqt.webview import AnkiWebView
 from . import config
 from . import settings
 
-CURRENT_WELCOME_VERSION = "1.0.8.2-beta"
+CURRENT_WELCOME_VERSION = "1.0.8.3-beta"
 
 class WelcomeDialog(QDialog):
     """
